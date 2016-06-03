91757cb3565b7e2a92c6ee560981bb701602db57
Cédric Eugeni <cedric.eugeni@gmail.com>
2016-06-03 17:34:54 +0200

    Added iProgress framework project Added iProgress Demo app project Configured Demo project with framework dependency

---
+++ Demo/iProgress-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iProgress-Demo
+//
+//  Created by Cédric Eugeni on 03/06/2016.
+//  Copyright © 2016 iProgress. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iProgress/iProgress.h
@@ -0,0 +1,19 @@
+//
+//  iProgress.h
+//  iProgress
+//
+//  Created by Cédric Eugeni on 03/06/2016.
+//  Copyright © 2016 iProgress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iProgress.
+FOUNDATION_EXPORT double iProgressVersionNumber;
+
+//! Project version string for iProgress.
+FOUNDATION_EXPORT const unsigned char iProgressVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iProgress/PublicHeader.h>
+
+
